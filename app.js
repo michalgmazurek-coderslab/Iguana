32b65fe4d174178917684baa730dbdd5695f6b49
Michał Mazurek <michalgmazurek@gmail.com>
2017-06-15 23:44:38 +0200

    ready

---
+++ app.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    var navTrigger = document.getElementById('nav-trigger');
+    var menuList = document.getElementsByClassName('menu__list');
+    var menuContainer = document.getElementsByClassName('menu__container')[0];
+
+    menuAnimation();
+
+    function menuAnimation() {
+        var navContainer = document.getElementsByClassName('container--nav');
+        console.log(menuContainer);
+        navTrigger.addEventListener('click', function(event) {
+            event.stopPropagation();
+            navTrigger.classList.toggle('active');
+            menuList[0].classList.toggle('list-visable');
+            menuContainer.classList.toggle('menu-higher');
+            navContainer[0].classList.toggle('menu-higher');
+            document.addEventListener('click', function() {
+                menuContainer.classList.remove('menu-higher');
+                navTrigger.classList.remove('active');
+                navContainer[0].classList.remove('menu-higher');
+                menuList[0].classList.remove('list-visable');
+            });
+
+        });
+    }
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            console.log(target.offset().top);
+            if ($(window).width() < 768) {
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top - 60
+                }, 1000);
+            } else {
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top - 90
+                }, 1000);
+            }
+        }
+    });
+});
